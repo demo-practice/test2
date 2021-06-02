75a68beff23eb9093e779e54f5d2a94561e2b247
manishnangliya <manunangliya@gmail.com>
2021-06-03 02:42:34 +0530

    db update

--- market/models.py
+++ market/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -20,3 +21,4 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+
